d80d413bcd6d95dffff792d04040bc3c492b3400
AslamNS <nsaslam55@gmail.com>
2024-07-09 11:06:48 +0530

    Deleted backend

--- BackEnd/sportsarena/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
-Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
